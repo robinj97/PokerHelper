eb326653cf037f0625dc970d07858460379d9b8b
Robin Jain <robinjain97@gmail.com>
2018-01-23 20:02:12 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Class to represent a single card.
+ */
+public class Card {
+  private static String[] values = {"Ace","2","3","4","5","6",
+  "7","8","9","10","Jack","Queen", "King"};
+  private static String[] suits = { "Diamonds", "Hearts", "Clubs", "Spades"};
+
+}
