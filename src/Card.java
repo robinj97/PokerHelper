fb00a335edff5624ae75c2274e26f2d28e61dced
Robin Jain <robinjain97@gmail.com>
2018-01-23 20:17:27 -0500

    Added some body to the class
    Added toString function and getValue function.

--- src/Card.java
+++ src/Card.java
@@ -9,4 +9,40 @@ public class Card {
   "7","8","9","10","Jack","Queen", "King"};
   private static String[] suits = { "Diamonds", "Hearts", "Clubs", "Spades"};
 
+  private int value;
+  private int suit;
+
+  /**
+   *Public constructor for card.
+   * @param value the value of the card.
+   * @param suit suit of the card.
+   */
+  public Card(int value, int suit) {
+    this.value = value;
+    this.suit = suit;
+  }
+
+  /**
+   * ToString method for a card.
+   */
+  public String toString() {
+    StringBuilder s = new StringBuilder();
+    s.append(values[value]);
+    s.append(" of ");
+    s.append(suits[suit]);
+    return s.toString();
+  }
+
+  /**
+   * Getter method for the value of the card.
+   * @return int.
+   */
+  public int getValue() {
+    return value;
+  }
+
+  public int getSuit() {
+    
+  }
+
 }
